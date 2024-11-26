d26f3b0ac2bc22ed046f58555711402fd79ad252
hyunjoonkang <hyunjoonkang01@gmail.com>
2024-11-26 18:26:52 +0900

    temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <h1>Suffix Array Visualizer</h1>{" "}
       {/* 이 줄 추가 - 화면에 뭐라도 보이는지 테스트 */}
       <SuffixArrayVisualizer />
     </div>
